e158d73a80f2bd47145d09cbf0d7c0c775ecf646
MatteP1 <mathiasp9999@gmail.com>
2025-12-15 23:40:28 +0100

    added signing key setup

--- home/git.nix
+++ home/git.nix
@@ -3,6 +3,12 @@
 {
 	programs.git = {
 		enable = true;
+		signing = {
+			format = "ssh";
+			key = "~/.ssh/signing-key.pub";
+			# Note: the signing key must be created manually. E.g. using `ssh-keygen`.
+			signByDefault = true;
+		};
 		settings = {
 			user = {
 				name = "MatteP1";
