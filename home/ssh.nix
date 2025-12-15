52fd04961df08792150df6122b620e0c0aca27cc
MatteP1 <mathiasp9999@gmail.com>
2025-12-16 00:05:29 +0100

    updated ssh config

--- home/ssh.nix
+++ home/ssh.nix
@@ -1,7 +1,20 @@
 { ... }:
 
 {
-  programs.ssh = {
-    enable = true;
-  };
+	programs.ssh = {
+		enable = true;
+		enableDefaultConfig = false;
+		matchBlocks."*" = {
+			forwardAgent = false;
+			addKeysToAgent = "confirm";
+			compression = false;
+			serverAliveInterval = 0;
+			serverAliveCountMax = 3;
+			hashKnownHosts = false;
+			userKnownHostsFile = "~/.ssh/known_hosts"; 
+			controlMaster = "no";
+			controlPath = "~/.ssh/master-%r@%n:%p";
+			controlPersist = "no";
+		};
+	};
 }
