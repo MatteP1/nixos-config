8e7b477d443ab36040534c327296a4d3a779e814
MatteP1 <mathiasp9999@gmail.com>
2025-12-21 22:51:31 +0100

    keybindings and more config

--- home/kitty.nix
+++ home/kitty.nix
@@ -5,12 +5,26 @@
     shellIntegration.enableFishIntegration = true;
     settings = {
       confirm_os_window_close = 0;
+      cursor_shape = "block";
+      cursor_trail = 1;
+      scrollback_lines = 100000;
+      strip_trailing_spaces = "smart";
       dynamic_background_opacity = true;
       font_family = "FiraCode Nerd Font";
       font_size = "11";
       background_opacity = "0.7";
       shell = "fish";
       editor = "nvim";
+    };
+
+    keybindings = {
+	"kitty_mod+0" = "change_font_size all 0";
+	"kitty_mod+=" = "change_font_size all +1";
+	"kitty_mod+-" = "change_font_size all -1";
+	"kitty_mod+с" = "copy_to_clipboard";
+	"kitty_mod+v" = "paste_from_clipboard";
+	"page_up" = "scroll_page_up";
+	"page_down" = "scroll_page_down";
     };
   };
 }
