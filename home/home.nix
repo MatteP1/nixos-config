3726c0a0c5f9ed23d0168a0d0fb770b083f61605
MatteP1 <mathiasp9999@gmail.com>
2025-12-21 22:12:18 +0100

    configured kitty

--- home/home.nix
+++ home/home.nix
@@ -7,6 +7,7 @@
   programs.home-manager.enable = true;
 
   imports = [
+    ./kitty.nix
     ./neovim.nix
     ./fcitx5.nix
     ./ssh.nix
---
+++ home/kitty.nix
@@ -0,0 +1,15 @@
+{pkgs, ...}: {
+  programs.kitty = {
+    enable = true;
+    # themeFile = "Catppuccin-Mocha";
+    shellIntegration.enableFishIntegration = true;
+    settings = {
+      confirm_os_window_close = 0;
+      dynamic_background_opacity = true;
+      font_family = "FiraCode Nerd Font";
+      background_opacity = "0.7";
+      shell = "fish";
+      editor = "nvim";
+    };
+  };
+}
