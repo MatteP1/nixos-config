85bfcca12a709517a6cf9dabe0800e10311dd0e5
MatteP1 <mathiasp9999@gmail.com>
2025-12-21 22:15:47 +0100

    font size

--- home/kitty.nix
+++ home/kitty.nix
@@ -7,6 +7,7 @@
       confirm_os_window_close = 0;
       dynamic_background_opacity = true;
       font_family = "FiraCode Nerd Font";
+      font_size = "11";
       background_opacity = "0.7";
       shell = "fish";
       editor = "nvim";
